16a1aae89df7fabcd64b1fe617fd9be0692e137f
maki <maki@hotmilk.space>
2025-10-07 13:48:07 -0400

    Allow using words for class names

--- context.go
+++ context.go
@@ -2,12 +2,20 @@ package goemo
 
 import (
 	"context"
+	"hash/crc64"
 	"log/slog"
+	"math/rand"
+	"slices"
 	"strings"
+	"unsafe"
 )
 
 var (
 	pageStylesKey = "goemoPageStyles"
+
+	usingHashWords bool
+	hashWords      []string
+	hashWordsMap   = map[string]uint{}
 )
 
 type pageStyle struct {
@@ -17,6 +25,34 @@ type pageStyle struct {
 
 func InitContext(parent context.Context) context.Context {
 	return context.WithValue(parent, pageStylesKey, &[]pageStyle{})
+}
+
+func UseWords(words []string, seedString string) {
+	usingHashWords = true
+	hashWords = []string{}
+
+	for _, word := range words {
+		if len(word) == 0 {
+			continue
+		}
+
+		word = strings.TrimSpace(word)
+		word = strings.ToLower(word)
+		word = strings.ReplaceAll(word, " ", "-")
+
+		if !slices.Contains(hashWords, word) {
+			hashWords = append(hashWords, word)
+		}
+	}
+
+	seedUint := crc64.Checksum([]byte(seedString), crc64.MakeTable(crc64.ECMA))
+
+	r := rand.New(rand.NewSource(*(*int64)(unsafe.Pointer(&seedUint))))
+
+	for i := len(hashWords) - 1; i > 0; i-- {
+		j := r.Intn(i + 1)
+		hashWords[i], hashWords[j] = hashWords[j], hashWords[i]
+	}
 }
 
 // returns class name and injects scss into page
@@ -32,7 +68,16 @@ func SCSS(ctx context.Context, snippet string) string {
 	}
 
 	// TODO: snippet doesnt consider whitespace
-	className := hashString(snippet)
+	var className = hashString(snippet)
+
+	if usingHashWords {
+		hashWordIndex, exists := hashWordsMap[className]
+		if !exists {
+			hashWordIndex = uint(len(hashWordsMap))
+			hashWordsMap[className] = hashWordIndex
+		}
+		className = hashWords[hashWordIndex]
+	}
 
 	for _, style := range *pageStyles {
 		if style.ClassName == className {
