8eab255081d2bf9dd2c96fea5c7792aea15e4275
maki <maki@hotmilk.space>
2025-10-31 15:30:24 -0400

    Improve emohtml stack

--- emohtml/stack.go
+++ emohtml/stack.go
@@ -2,15 +2,22 @@ package emohtml
 
 import (
 	"context"
-	"strings"
+	"io"
 
 	"github.com/makinori/goemo"
 	. "maragu.dev/gomponents"
 	. "maragu.dev/gomponents/html"
 )
 
+// nice! if only we can do this for goemo itself lol
+type StackSCSS string
+
+func (_ StackSCSS) Render(_ io.Writer) error {
+	return nil
+}
+
 func stack(
-	ctx context.Context, flexDir string, args []Node, extraSCSS ...string,
+	ctx context.Context, flexDir string, children ...Node,
 ) Node {
 	class := goemo.SCSS(ctx, `
 		display: flex;
@@ -18,19 +25,22 @@ func stack(
 		gap: 8px;
 	`)
 
-	if len(extraSCSS) > 0 {
-		class += " " + goemo.SCSS(ctx, strings.Join(extraSCSS, "\n"))
+	for _, node := range children {
+		switch scss := node.(type) {
+		case StackSCSS:
+			class += " " + goemo.SCSS(ctx, string(scss))
+		}
 	}
 
 	return Div(
 		Class(class),
-		Group(args),
+		Group(children),
 	)
 }
-func HStack(ctx context.Context, args []Node, extraSCSS ...string) Node {
-	return stack(ctx, "row", args, extraSCSS...)
+func HStack(ctx context.Context, children ...Node) Node {
+	return stack(ctx, "row", children...)
 }
 
-func VStack(ctx context.Context, args []Node, extraSCSS ...string) Node {
-	return stack(ctx, "column", args, extraSCSS...)
+func VStack(ctx context.Context, children ...Node) Node {
+	return stack(ctx, "column", children...)
 }
